3b2d3cfdee03fa0d278a3f37ff85b5b9e67451d6
Richard Michael Coo <richard.michael.coo@gmail.com>
2022-05-06 12:07:51 +0800

    🎨 codestyle: run Prettier on all frontend code

--- frontend/src/components/ExampleComponent.vue
+++ frontend/src/components/ExampleComponent.vue
@@ -13,29 +13,22 @@
 </template>
 
 <script lang="ts">
-import {
-  defineComponent,
-  PropType,
-  computed,
-  ref,
-  toRef,
-  Ref,
-} from 'vue';
-import { Todo, Meta } from './models';
+import { defineComponent, PropType, computed, ref, toRef, Ref } from 'vue'
+import { Todo, Meta } from './models'
 
 function useClickCount() {
-  const clickCount = ref(0);
+  const clickCount = ref(0)
   function increment() {
     clickCount.value += 1
-    return clickCount.value;
+    return clickCount.value
   }
 
-  return { clickCount, increment };
+  return { clickCount, increment }
 }
 
 function useDisplayTodo(todos: Ref<Todo[]>) {
-  const todoCount = computed(() => todos.value.length);
-  return { todoCount };
+  const todoCount = computed(() => todos.value.length)
+  return { todoCount }
 }
 
 export default defineComponent({
@@ -43,22 +36,22 @@ export default defineComponent({
   props: {
     title: {
       type: String,
-      required: true
+      required: true,
     },
     todos: {
       type: Array as PropType<Todo[]>,
-      default: () => []
+      default: () => [],
     },
     meta: {
       type: Object as PropType<Meta>,
-      required: true
+      required: true,
     },
     active: {
-      type: Boolean
-    }
+      type: Boolean,
+    },
   },
-  setup (props) {
-    return { ...useClickCount(), ...useDisplayTodo(toRef(props, 'todos')) };
+  setup(props) {
+    return { ...useClickCount(), ...useDisplayTodo(toRef(props, 'todos')) }
   },
-});
+})
 </script>
