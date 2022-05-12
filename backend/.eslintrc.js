88795855eecb0809f9d0e36c3a0383e5a55c12c4
Richard Michael Coo <richard.michael.coo@gmail.com>
2022-05-12 15:05:51 +0800

    🔧 config: require return type annotation in ESLint

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -17,7 +17,7 @@ module.exports = {
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'error',
     '@typescript-eslint/no-explicit-any': 'off',
 
     // https://basarat.gitbook.io/typescript/main-1/defaultisbad
