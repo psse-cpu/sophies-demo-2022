0a4d33dc7d397a96edd29699a49f742c2d19a223
Richard Michael Coo <richard.michael.coo@gmail.com>
2022-05-13 01:22:24 +0800

    🗃️ db: add users table

---
+++ backend/migrations/1652375904216-AddUsersTable.ts
@@ -0,0 +1,23 @@
+import { MigrationInterface, QueryRunner } from 'typeorm'
+
+export class AddUsersTable1652375904216 implements MigrationInterface {
+    name = 'AddUsersTable1652375904216'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            CREATE TABLE "user" (
+                "id" SERIAL NOT NULL,
+                "email" character varying NOT NULL,
+                "password_hash" text NOT NULL,
+                CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email"),
+                CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id")
+            )
+        `)
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            DROP TABLE "user"
+        `)
+    }
+}
