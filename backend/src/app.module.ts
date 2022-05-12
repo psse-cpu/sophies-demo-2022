bd789bd5dd881edb84386f2bd7d2d7811acbdc42
Richard Michael Coo <richard.michael.coo@gmail.com>
2022-05-12 14:40:23 +0800

    ♻️ refactor: migrate app.module and main.ts from dotenv to @nestjs/config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { ConfigModule } from '@nestjs/config'
 
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
@@ -8,6 +9,7 @@ import { UsersModule } from './users/users.module'
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: process.env.DATABASE_URL,
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,7 @@
-import dotenv from 'dotenv'
-
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  dotenv.config()
-
   const app = await NestFactory.create(AppModule)
   await app.listen(3000)
 }
