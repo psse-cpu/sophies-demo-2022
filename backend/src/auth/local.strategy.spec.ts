6fffb61237b30d873397689e3d55251d16becef4
Richard Michael Coo <richard.michael.coo@gmail.com>
2022-05-12 22:20:14 +0800

    ✨ feature: add local auth strategy

---
+++ backend/src/auth/local.strategy.spec.ts
@@ -0,0 +1,52 @@
+import { UnauthorizedException } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import { UserWithoutHash } from '../users/user.entity'
+import { AuthService } from './auth.service'
+import { LocalStrategy } from './local.strategy'
+
+const mockUser = { id: 1, email: 'mike@foo.bar' }
+
+describe('LocalStrategy', () => {
+  let localStrategy: LocalStrategy
+  let authService: AuthService
+
+  beforeEach(async () => {
+    const MockAuthServiceDef = {
+      provide: AuthService,
+      useValue: {
+        async authenticate(
+          _email: string,
+          _password: string
+        ): Promise<UserWithoutHash> {
+          return mockUser
+        },
+      },
+    }
+
+    const module = await Test.createTestingModule({
+      providers: [LocalStrategy, MockAuthServiceDef],
+    }).compile()
+
+    localStrategy = module.get(LocalStrategy)
+    authService = module.get(AuthService)
+  })
+
+  it('should be defined', () => {
+    expect(localStrategy).toBeDefined()
+  })
+
+  describe('#validate', () => {
+    it('returns a user when authService#authenticate does so', async () => {
+      const result = await localStrategy.validate('mike@foo.bar', 'whatever')
+      return expect(result).toBe(mockUser)
+    })
+
+    it('throws an UnauthorizedException when authService#authenticate is falsy', async () => {
+      jest.spyOn(authService, 'authenticate').mockResolvedValue(undefined)
+
+      return expect(() =>
+        localStrategy.validate('mike@foo.bar', 'whatever')
+      ).rejects.toThrow(UnauthorizedException)
+    })
+  })
+})
---
+++ backend/src/auth/local.strategy.ts
@@ -0,0 +1,23 @@
+import { Strategy } from 'passport-local'
+import { PassportStrategy } from '@nestjs/passport'
+import { Injectable, UnauthorizedException } from '@nestjs/common'
+import { User } from 'src/users/user.entity'
+import { AuthService } from './auth.service'
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: AuthService) {
+    super({ usernameField: 'email' })
+  }
+
+  async validate(
+    username: string,
+    password: string
+  ): Promise<Omit<User, 'passwordHash'>> {
+    const user = await this.authService.authenticate(username, password)
+    if (!user) {
+      throw new UnauthorizedException()
+    }
+    return user
+  }
+}
