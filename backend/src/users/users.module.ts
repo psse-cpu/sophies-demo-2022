4d310614a13f9347ae89b44d19cccc299c104b3e
Richard Michael Coo <richard.michael.coo@gmail.com>
2022-05-12 18:18:24 +0800

    🩹 fix: fix missing exports in UsersModule

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -6,5 +6,6 @@ import { UsersService } from './users.service'
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
