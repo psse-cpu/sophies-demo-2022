68e8a121f0f9bbc94885d3aea3bd147817189fb2
Richard Michael Coo <richard.michael.coo@gmail.com>
2022-05-12 18:02:38 +0800

    ✨ feature: add simple local auth service

--- backend/src/auth/auth.service.spec.ts
+++ backend/src/auth/auth.service.spec.ts
@@ -1,18 +1,66 @@
 import { Test, TestingModule } from '@nestjs/testing'
+
+import { User } from '../users/user.entity'
+import { UsersService } from '../users/users.service'
 import { AuthService } from './auth.service'
 
+const mockUser: User = { id: 1, email: 'mike@foo.bar', passwordHash: 'hash' }
+
+jest.mock('bcrypt', () => ({
+  compare: async (_plain: string, _hashed: string) => _plain === 'correct',
+}))
+
 describe('AuthService', () => {
-  let service: AuthService
+  let authService: AuthService
+  let usersService: UsersService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: {
+            findByEmail: jest.fn(),
+          },
+        },
+      ],
     }).compile()
 
-    service = module.get<AuthService>(AuthService)
+    authService = module.get(AuthService)
+    usersService = module.get(UsersService)
   })
 
   it('should be defined', () => {
-    expect(service).toBeDefined()
+    expect(authService).toBeDefined()
+  })
+
+  describe('#authenticate', () => {
+    it('returns undefined if username is invalid (undefined user)', async () => {
+      jest.spyOn(usersService, 'findByEmail').mockResolvedValue(undefined)
+      const result = await authService.authenticate('mike@foo.bar', 'correct')
+      expect(result).toBe(undefined)
+    })
+
+    it('returns undefined for a valid email (truthy user), but invalid password', async () => {
+      jest.spyOn(usersService, 'findByEmail').mockResolvedValue(mockUser)
+
+      const result = await authService.authenticate('mike@foo.bar', 'wrong')
+      expect(result).toBeUndefined()
+    })
+
+    it('returns undefined for a valid email (truthy user) and correct password', async () => {
+      jest.spyOn(usersService, 'findByEmail').mockResolvedValue(mockUser)
+
+      const result = await authService.authenticate('mike@foo.bar', 'correct')
+      expect(result).toEqual({ id: 1, email: 'mike@foo.bar' })
+    })
+
+    it('returns users without the passwordHash field', async () => {
+      jest.spyOn(usersService, 'findByEmail').mockResolvedValue(mockUser)
+
+      const result = await authService.authenticate('mike@foo.bar', 'correct')
+      expect(result).not.toHaveProperty('passwordHash')
+    })
   })
 })
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common'
+import bcrypt from 'bcrypt'
+import omit from 'lodash/omit'
+
+import { User } from '../users/user.entity'
+import { UsersService } from '../users/users.service'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async authenticate(
+    email: string,
+    password: string
+  ): Promise<Omit<User, 'passwordHash'> | undefined> {
+    const user = await this.usersService.findByEmail(email)
+
+    if (!user) {
+      return undefined
+    }
+
+    const hashesMatch = await bcrypt.compare(password, user.passwordHash)
+    return hashesMatch ? omit(user, 'passwordHash') : undefined
+  }
+}
