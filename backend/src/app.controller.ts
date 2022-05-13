afc2270d97b133bcec765bbef6ee50443ff26613
Richard Michael Coo <richard.michael.coo@gmail.com>
2022-05-14 01:32:52 +0800

    🔥 prune: remove AppService dependency in AppController

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,9 @@
 import { Controller, Req, Post, UseGuards } from '@nestjs/common'
 import { Express } from 'express'
-import { AppService } from './app.service'
 import { LocalAuthGuard } from './auth/local-auth.guard'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(
