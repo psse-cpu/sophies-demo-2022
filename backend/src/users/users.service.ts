dcf870e782286f159de3a8ab229012ec562e7528
Richard Michael Coo <richard.michael.coo@gmail.com>
2022-05-14 01:34:21 +0800

    ✨ feature: add register method to UsersService

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,5 @@
-import { Repository } from 'typeorm'
+import bcrypt from 'bcrypt'
+import { InsertResult, Repository } from 'typeorm'
 import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { User } from './user.entity'
@@ -13,5 +14,14 @@ export class UsersService {
     return this.userRepository.findOne({
       where: { email },
     })
+  }
+
+  // TODO: this method has no unit tests
+  async register(
+    email: string,
+    plainTextPassword: string
+  ): Promise<InsertResult> {
+    const passwordHash = await bcrypt.hash(plainTextPassword, 10)
+    return this.userRepository.insert({ email, passwordHash })
   }
 }
