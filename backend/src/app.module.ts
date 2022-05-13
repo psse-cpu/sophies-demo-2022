5fd079cfb00c371a46304f4d27e376c8142fda1c
Richard Michael Coo <richard.michael.coo@gmail.com>
2022-05-14 01:31:13 +0800

    ✨ feature: enable AppModule to use sqlite3 in-memory DB during tests

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
-import { ConfigModule } from '@nestjs/config'
-
 import omit from 'lodash/omit'
+
 import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
 import { UsersModule } from './users/users.module'
 
@@ -12,12 +10,20 @@ import ormConfig from '../ormconfig'
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot(omit(ormConfig, 'cli')),
+    TypeOrmModule.forRoot(
+      process.env.NODE_ENV === 'test'
+        ? {
+            type: 'better-sqlite3',
+            database: ':memory:',
+            autoLoadEntities: true,
+            dropSchema: true,
+            synchronize: true,
+          }
+        : omit(ormConfig, 'cli')
+    ),
     AuthModule,
     UsersModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
