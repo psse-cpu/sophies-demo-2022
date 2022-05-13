f889341874c3f8be55b48cadce7d44512ec38233
Richard Michael Coo <richard.michael.coo@gmail.com>
2022-05-14 01:37:09 +0800

    ✅ tests: add integration test for /auth/login

--- backend/test/app.integration.spec.ts
+++ backend/test/app.integration.spec.ts
@@ -1,22 +1,38 @@
-import { Test, TestingModule } from '@nestjs/testing'
 import { INestApplication } from '@nestjs/common'
+import { Test, TestingModule } from '@nestjs/testing'
 import request from 'supertest'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppModule } from '../src/app.module'
+import { User } from '../src/users/user.entity'
+import { UsersService } from '../src/users/users.service'
+
+const seedDatabase = async (usersService: UsersService) => {
+  return Promise.all([
+    usersService.register('mike@foo.bar', 'like'),
+    usersService.register('fifi@foo.bar', 'dog'),
+  ])
+}
 
 describe('AppController (e2e)', () => {
   let app: INestApplication
+  let usersService: UsersService
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, TypeOrmModule.forFeature([User])],
+      providers: [UsersService],
     }).compile()
 
     app = moduleFixture.createNestApplication()
     await app.init()
+
+    usersService = moduleFixture.get(UsersService)
+
+    await seedDatabase(usersService)
   })
 
-  afterAll(() => {
-    app.close()
+  afterEach(() => {
+    return app.close()
   })
 
   describe('/auth/login', () => {
@@ -24,10 +40,43 @@ describe('AppController (e2e)', () => {
       return request(app.getHttpServer())
         .post('/auth/login')
         .send({
-          email: 'mike@foo.bar',
+          email: 'bbm@gov.ph',
           password: 'like',
         })
         .expect(401)
+    })
+
+    it('returns 401 with wrong credentials (bad username)', () => {
+      return request(app.getHttpServer())
+        .post('/auth/login')
+        .send({
+          email: 'bbm@gov.ph',
+          password: 'like',
+        })
+        .expect(401)
+    })
+
+    it('returns 401 with wrong credentials (bad password)', () => {
+      return request(app.getHttpServer())
+        .post('/auth/login')
+        .send({
+          email: 'mike@gov.ph',
+          password: 'wrong',
+        })
+        .expect(401)
+    })
+
+    it('returns 200 for correct credentials', async () => {
+      return request(app.getHttpServer())
+        .post('/auth/login')
+        .send({
+          email: 'mike@foo.bar',
+          password: 'like',
+        })
+        .expect(201)
+        .then((response) => {
+          expect((response.body as User).email).toBe('mike@foo.bar')
+        })
     })
   })
 })
