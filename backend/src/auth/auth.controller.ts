7fbee1223316e2650998817c9caf963e27ac3e9b
Richard Michael Coo <richard.michael.coo@gmail.com>
2022-05-16 00:14:56 +0800

    ✨ feature: add Google auth methods for controller

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Req, Post, UseGuards } from '@nestjs/common'
+import { Controller, Req, Post, UseGuards, Get } from '@nestjs/common'
 import { Express } from 'express'
+import { GoogleOAuthGuard } from './google-oauth.guard'
 import { LocalAuthGuard } from './local-auth.guard'
 
 @Controller('/auth')
@@ -7,6 +8,18 @@ export class AuthController {
   @UseGuards(LocalAuthGuard)
   @Post('/login')
   async login(
+    @Req() request: Express.Request
+  ): Promise<Express.User | undefined> {
+    return request.user
+  }
+
+  @Get('/google')
+  @UseGuards(GoogleOAuthGuard)
+  async googleLogin(@Req() _request: Express.Request): Promise<void> {}
+
+  @Get('/google/redirect')
+  @UseGuards(GoogleOAuthGuard)
+  async googleLoginRedicect(
     @Req() request: Express.Request
   ): Promise<Express.User | undefined> {
     return request.user
