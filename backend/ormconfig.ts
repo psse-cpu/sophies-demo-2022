f49691c6c3c9936648b8221a08aacf829f53c9ab
Richard Michael Coo <richard.michael.coo@gmail.com>
2022-05-17 16:35:38 +0800

    ➕ depadd: add and configure typeorm-uml

--- backend/ormconfig.ts
+++ backend/ormconfig.ts
@@ -3,13 +3,15 @@ import 'dotenv/config'
 
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions'
 
-// eslint-disable-next-line import/no-default-export
+// eslint-disable-next-line import/no-default-export -- it's a config file!
 export default {
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  entities: [`${__dirname}/**/*.entity.{ts,js}`],
-  migrations: ['database/migrations/*'],
   namingStrategy: new SnakeNamingStrategy(),
+
+  // eslint-disable-next-line unicorn/prefer-module -- project uses CommonJS
+  entities: [`${__dirname}/src/**/*.entity.{ts,js}`],
+  migrations: ['database/migrations/*'],
   cli: {
     migrationsDir: 'migrations',
   },
