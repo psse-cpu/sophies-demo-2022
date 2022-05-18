384bcbe715f2f5b2d4aa1f1e8aeb0091d90213fe
Richard Michael Coo <richard.michael.coo@gmail.com>
2022-05-18 15:39:29 +0800

    ✅ tests: add test for user and exercise resolvers

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-![Statements](https://img.shields.io/badge/Backend%20Code%20Coverage-64.33%25-red.svg?style=flat&logo=jest)
+![Statements](https://img.shields.io/badge/Backend%20Code%20Coverage-75.62%25-red.svg?style=flat&logo=jest)
 ![example workflow](https://github.com/psse-cpu/sophies-demo-2022/actions/workflows/node.js.yml/badge.svg)
 
 # Sophies Demo
--- backend/src/users/user.resolver.spec.ts
+++ backend/src/users/user.resolver.spec.ts
@@ -1,4 +1,39 @@
-// pass eslint
-describe('wip', () => {
-  it('passes', () => expect(1).toBe(1))
+import { Test } from '@nestjs/testing'
+import { UserWithoutHash } from './user.entity'
+import { UserResolver } from './user.resolver'
+import { UsersService } from './users.service'
+
+const mockUser: UserWithoutHash = {
+  id: 1,
+  email: 'fifi@cpu.ph',
+}
+
+const mockUsersService = {
+  allUsers: jest.fn().mockResolvedValue([mockUser]),
+}
+
+describe('UserResolver', () => {
+  let resolver: UserResolver
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        UserResolver,
+        {
+          provide: UsersService,
+          useValue: mockUsersService,
+        },
+      ],
+    }).compile()
+
+    resolver = module.get(UserResolver)
+  })
+
+  describe('#users()', () => {
+    it('returns all users', () => {
+      return expect(resolver.users()).resolves.toStrictEqual([
+        { id: 1, email: 'fifi@cpu.ph' },
+      ])
+    })
+  })
 })
