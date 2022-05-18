e3e561a94b21abf4cb697184065ab421a8b9de4d
Richard Michael Coo <richard.michael.coo@gmail.com>
2022-05-18 15:40:57 +0800

    🛂 auth: guard UserResolver

--- backend/src/users/user.resolver.ts
+++ backend/src/users/user.resolver.ts
@@ -1,7 +1,10 @@
+import { UseGuards } from '@nestjs/common'
 import { Query, Resolver } from '@nestjs/graphql'
+import { JwtGuard } from '../auth/jwt.guard'
 import { User, UserWithoutHash } from './user.entity'
 import { UsersService } from './users.service'
 
+@UseGuards(JwtGuard)
 @Resolver(() => User)
 export class UserResolver {
   constructor(private usersService: UsersService) {}
