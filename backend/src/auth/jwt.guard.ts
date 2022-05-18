99e179746ea90d32b90df35fa9c729a1ad6bf4d4
Richard Michael Coo <richard.michael.coo@gmail.com>
2022-05-18 15:40:13 +0800

    🐛 bugfix: fix JWT guard to be GraphQL-compatible

--- backend/src/auth/jwt.guard.ts
+++ backend/src/auth/jwt.guard.ts
@@ -1,5 +1,11 @@
-import { Injectable } from '@nestjs/common'
+import { ExecutionContext, Injectable } from '@nestjs/common'
+import { GqlExecutionContext } from '@nestjs/graphql'
 import { AuthGuard } from '@nestjs/passport'
 
 @Injectable()
-export class JwtGuard extends AuthGuard('jwt') {}
+export class JwtGuard extends AuthGuard('jwt') {
+  /* override */ getRequest(context: ExecutionContext): Express.Request {
+    const gqlContext = GqlExecutionContext.create(context)
+    return gqlContext.getContext().req
+  }
+}
