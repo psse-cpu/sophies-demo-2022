23b6203e1688e331e2420d9e5b1deb86200faea5
Richard Michael Coo <richard.michael.coo@gmail.com>
2022-05-25 02:06:23 +0800

    ✅ tests: add unit test for login form

---
+++ frontend/src/components/LoginForm.spec.ts
@@ -0,0 +1,132 @@
+import { flushPromises, mount } from '@vue/test-utils'
+import { Quasar } from 'quasar'
+import localforage from 'localforage'
+import { backend } from 'src/axios'
+import LoginForm from 'src/components/LoginForm.vue'
+
+const wrapperFactory = () =>
+  mount(LoginForm, {
+    global: {
+      plugins: [Quasar],
+    },
+  })
+
+const pushSpy = vi.fn()
+vi.mock('vue-router', () => ({
+  useRoute: () => ({
+    redirectedFrom: {
+      fullPath: '/somewhere',
+    },
+  }),
+  useRouter: vi.fn(() => ({
+    push: pushSpy,
+  })),
+}))
+
+vi.mock('src/auth-strategies/google-auth')
+
+const emailSelector = '[data-testid="email-input"]'
+const passwordSelector = '[data-testid="password-input"]'
+const formSelector = '[data-testid="login-form"]'
+const errorBoxSelector = '[data-testid="auth-errors"]'
+
+describe('LoginPage', () => {
+  afterEach(() => {
+    vi.clearAllMocks()
+  })
+
+  it('updates the model correctly', () => {
+    vi.spyOn(backend, 'post')
+
+    const wrapper = wrapperFactory()
+    wrapper.find(emailSelector).setValue('mike@cpu.edu.ph')
+    wrapper.find(passwordSelector).setValue('lol')
+
+    expect(wrapper.vm.credentials).toStrictEqual({
+      email: 'mike@cpu.edu.ph',
+      password: 'lol',
+    })
+  })
+
+  it('sends the correct args to Axios', async () => {
+    const spy = vi.spyOn(backend, 'post')
+
+    const wrapper = wrapperFactory()
+    wrapper.find(emailSelector).setValue('mike@cpu.edu.ph')
+    wrapper.find(passwordSelector).setValue('lol')
+
+    await wrapper.find(formSelector).trigger('submit')
+
+    expect(spy).toHaveBeenCalledWith('/auth/login', {
+      email: 'mike@cpu.edu.ph',
+      password: 'lol',
+    })
+  })
+
+  it('persists the current user to localStorage', async () => {
+    vi.spyOn(backend, 'post').mockResolvedValue({
+      data: {
+        id: 1,
+        email: 'mike@yahoo.com',
+      },
+    })
+    const spy = vi.spyOn(localforage, 'setItem')
+
+    const wrapper = wrapperFactory()
+    wrapper.find(emailSelector).setValue('mike@cpu.edu.ph')
+    wrapper.find(passwordSelector).setValue('lol')
+
+    await wrapper.find(formSelector).trigger('submit')
+
+    expect(spy).toHaveBeenCalledWith('currentUser', {
+      id: 1,
+      email: 'mike@yahoo.com',
+    })
+  })
+
+  it('redirects on successful sign-in', async () => {
+    vi.spyOn(backend, 'post').mockResolvedValue({
+      data: {
+        id: 1,
+        email: 'mike@yahoo.com',
+      },
+    })
+
+    const wrapper = wrapperFactory()
+    wrapper.find(emailSelector).setValue('mike@cpu.edu.ph')
+    wrapper.find(passwordSelector).setValue('lol')
+    await wrapper.find(formSelector).trigger('submit')
+
+    expect(pushSpy).toHaveBeenCalledWith('/somewhere')
+  })
+
+  it('shows an error message on invalid credentials', async () => {
+    vi.spyOn(backend, 'post').mockRejectedValue({
+      response: { status: 401 },
+    })
+
+    const wrapper = wrapperFactory()
+    wrapper.find(emailSelector).setValue('mike@cpu.edu.ph')
+    wrapper.find(passwordSelector).setValue('lol')
+    await wrapper.find(formSelector).trigger('submit')
+    await flushPromises()
+
+    expect(wrapper.find(errorBoxSelector).text()).toBe(
+      'Invalid username or password.'
+    )
+  })
+
+  it('shows an error message on some other unexpected error', async () => {
+    vi.spyOn(backend, 'post').mockRejectedValue({
+      response: { status: 500 },
+    })
+
+    const wrapper = wrapperFactory()
+    wrapper.find(emailSelector).setValue('mike@cpu.edu.ph')
+    wrapper.find(passwordSelector).setValue('lol')
+    await wrapper.find(formSelector).trigger('submit')
+    await flushPromises()
+
+    expect(wrapper.find(errorBoxSelector).text()).toContain('Unexpected error')
+  })
+})
