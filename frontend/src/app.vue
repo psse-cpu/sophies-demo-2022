9d136620581904451ac2ce15602ad58573e3b8f2
Richard Michael Coo <richard.michael.coo@gmail.com>
2022-05-30 00:37:50 +0800

    ✨ feature: add urql client provider

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -1,3 +1,13 @@
 <template>
   <router-view />
 </template>
+
+<script setup lang="ts">
+import { createClient, provideClient } from '@urql/vue'
+
+const client = createClient({
+  url: `${import.meta.env.VITE_BACKEND_ORIGIN}/graphql`,
+})
+
+provideClient(client)
+</script>
