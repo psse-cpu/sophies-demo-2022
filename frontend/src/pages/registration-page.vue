4e66f91805843ee2b8291c34892e66a1a4111a4a
Richard Michael Coo <richard.michael.coo@gmail.com>
2022-05-29 20:09:28 +0800

    ♻️ refactor: move inline style to CSS

--- frontend/src/pages/registration-page.vue
+++ frontend/src/pages/registration-page.vue
@@ -1,9 +1,6 @@
 <template>
   <q-page class="col q-pa-none bg-grey-4">
-    <div
-      class="row flex-center content-center"
-      style="height: calc(100vh - 101px)"
-    >
+    <div class="registration-form row flex-center content-center">
       <registration-form class="col-md-4 col-xs-12 shadow-4" />
     </div>
   </q-page>
@@ -12,3 +9,9 @@
 <script setup lang="ts">
 import RegistrationForm from 'src/components/registration-form.vue'
 </script>
+
+<style scoped lang="scss">
+.registration-form {
+  height: calc(100vh - 101px);
+}
+</style>
