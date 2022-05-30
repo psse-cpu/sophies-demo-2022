912714bdb0ca2181349c8c9004c92f74e6e4a245
Richard Michael Coo <richard.michael.coo@gmail.com>
2022-05-31 00:37:31 +0800

    💡 comment: document idea on solving testability issues with urql

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -9,5 +9,6 @@ const client = createClient({
   url: `${import.meta.env.VITE_BACKEND_ORIGIN}/graphql`,
 })
 
+// TODO: investigate calling this in src/boot (could be the cause of testability issues)
 provideClient(client)
 </script>
